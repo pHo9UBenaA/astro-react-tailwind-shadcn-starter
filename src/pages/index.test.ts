94e446d46129272fdd0367ca3643e3b849eced4f
pHo9UBenaA <102408484+pHo9UBenaA@users.noreply.github.com>
2025-02-16 15:36:40 +0900

    fix test

--- src/pages/index.test.ts
+++ src/pages/index.test.ts
@@ -3,6 +3,18 @@ import { experimental_AstroContainer as AstroContainer } from 'astro/container';
 import { expect, test } from 'vitest';
 import Index from './index.astro';
 
+expect.addSnapshotSerializer({
+	test: (val) => typeof val === 'string',
+	print: (val) => {
+		return (
+			(val as string)
+				// Remove `data-astro-source-file` attributes to ensure consistent snapshots across different environments.
+				// see: https://github.com/pHo9UBenaA/astro-react-tailwind-shadcn-starter/actions/runs/13352440761/job/37290691107?pr=10
+				.replace(/\s+data-astro-source-file="[^"]*"/g, '')
+		);
+	},
+});
+
 test('snapshot', async () => {
 	const container = await AstroContainer.create();
 
